2bac6ac3c3e59dea528202c2c2991847ff0f24be
XDbitionist <eugene.sakharov@gmail.com>
2023-03-23 21:56:14 +0800

    hbvm

--- vendingmachine/HotBeverageVendingMachine.java
+++ vendingmachine/HotBeverageVendingMachine.java
@@ -1,2 +1,28 @@
-package org.example.vendingmachine;public class HotBeverageVendingMachine {
+package org.example.vendingmachine;
+import java.util.ArrayList;
+
+class HotBeverageVendingMachine implements VendingMachine {
+    private ArrayList<HotBeverage> products;
+
+    public HotBeverageVendingMachine(ArrayList<HotBeverage> products) {
+        this.products = products;
+    }
+
+    // Перегружаем метод getProduct для учета температуры
+    public HotBeverage getProduct(String name, int volume, int temperature) {
+        for (HotBeverage product : products) {
+            if (product.getName().equals(name) && product.getVolume() == volume && product.getTemperature() == temperature) {
+                products.remove(product);
+                return product;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public HotBeverage getProduct(String name, int volume) {
+        return null;
+    }
+
 }
+
