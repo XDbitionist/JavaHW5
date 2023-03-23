ba97429aff83cada4bcac09fa5ed6ec3d7c6c139
XDbitionist <eugene.sakharov@gmail.com>
2023-03-23 21:56:38 +0800

    hd

--- vendingmachine/HotDrink.java
+++ vendingmachine/HotDrink.java
@@ -1,2 +1,16 @@
-package org.example.vendingmachine;public class HotDrink {
+package org.example.vendingmachine;
+
+class HotDrink extends HotBeverage {
+    private int temperature;
+
+    public HotDrink(String name, int volume, int temperature) {
+        super(name, volume, temperature);
+        this.temperature = temperature;
+    }
+
+    // Переопределяем геттер, чтобы возвращать значение дополнительного поля
+    @Override
+    public int getTemperature() {
+        return temperature;
+    }
 }
